bb0f6b568a8bff0b696481f221e83737a0e0174f
unknown <harshitmishra5348@gmail.com>
2024-02-12 11:53:05 +0530

    location update

--- src/redux/reducers/shiftReducer.js
+++ src/redux/reducers/shiftReducer.js
@@ -7,7 +7,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const initialState = {
   current: [],
   timer: 0,
-  startBreakTime:""
+  startBreakTime: '',
+  location: {},
 };
 
 export const shiftReducer = (state = initialState, action) => {
@@ -34,16 +35,22 @@ export const shiftReducer = (state = initialState, action) => {
         timer: 0,
       };
 
-      case 'SET_START_BREAK_TIME':
+    case 'SET_START_BREAK_TIME':
       console.log('hey SET_START_BREAK_TIME :', action.payload);
 
       return {
         ...state,
-        timer:0,
+        timer: 0,
         startBreakTime: action.payload,
       };
 
+    case 'SET_LOCATION':
+      console.log('hey SET_LOCATION :', action.payload);
 
+      return {
+        ...state,
+        location: action.payload,
+      };
 
     default:
       return state;
